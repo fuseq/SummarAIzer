9d6520caf6109b87af144bc7e596337ec199c97d
Furkan Şenoğlu <20190808076@ogr.akdeniz.edu.tr>
2023-08-15 20:10:15 +0300

    minor changes

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,13 +6,13 @@ const Hero = () => {
     <header className='w-full flex justify-center items-center flex-col'>
 
       <nav className='flex justify-between items-center w-full mb-10 pt-3'>
-        <img src={logo} alt='logo' className='w-28 object-contain' />
+        <img src='/logo.png' alt='logo' className='w-16 object-contain' />
 
         <button type='button' onClick={() => window.open('https://github.com/fuseq')} className='black_btn' >Github</button>
       </nav>
 
       <h1 className='head_text'>
-        Summarize Articles with <br className='max-md:hidden'/>
+        Summarize Articles with <br className='max-md:hidden' />
         <span className='purple_gradient'>OpenAI GPT-4</span>
       </h1>
       <h2 className='desc'>
